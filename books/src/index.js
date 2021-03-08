28283a1ff37ec89c02f7a1676d884fbe8fe9b827
809Gagan <809gagan@gmail.com>
2021-03-08 18:19:40 +0530

    Learned Props

--- books/src/index.js
+++ books/src/index.js
@@ -4,50 +4,57 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import "./index.css";
 
+
+/*
+Thiskind of Books willl be of same properties 
+that is these books have same value of author name, tittle , same image;
+If we want to change these properties that is 
+if we want to make these properties book specific then we need to pass the properties as an object
+to thefunction component (Actually react passes this props object auttomatically to the function compopnent
+    all we have to do is add new key an values)
+
+    for eg : <Book Author = "Kali charan"/>
+*/
+
+
 const BookList = () => {
     return (
         <React.Fragment>
-            <Book />
-            <Book />
-            <Book />
-            <Book />
+            <Book AuthorName="Gagan" />
+            <Book AuthorName="Harjot" />
+            <Book AuthorName="Balveer" />
+            <Book AuthorName="Jagnoor" />
         </React.Fragment>
     );
 }
 
-const Book = () => {
+const Book = (props) => {
     return (
         <div className="book" >
-            <h1>Book</h1>
-            <Tittle />
             <Image />
-            <AuthorName />
+            <Tittle />
+            <h4>Author: {props.AuthorName}</h4>
         </div>
     );
 }
 
 const Tittle = () => {
     return (
-        <h4>Tittle of the book.</h4>
-    );
-}
-
-const AuthorName = () => {
-    return (
-        <h4>Author Name</h4>
+        <h4 style={{ fontSize: "20px" }}>Tittle of the book.</h4>
     );
 }
 
 const Image = () => {
     return (
-        <img style={{ height: 100, width: 100 }} src="/logo192.png" alt="NOT LOADED" />
+        <img id="bookImage" src="/book.jpeg" alt="NOT LOADED" />
         //in style = {{height:100}} first curly braces is for emntering into javas
         //JavaScripts Mode where as the second braces id for object
         /*
             we can also use 
             styleObject = {
                 height : 100,
-                color : #fff
+                color : '#fff',
+                property : "value"
             }
             style = {styleObject}
         */
