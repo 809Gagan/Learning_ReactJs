145d3339feff5dd5d024e5354cc7c0d6da99bb10
809Gagan <809gagan@gmail.com>
2021-03-08 17:31:26 +0530

    Added some code

--- books/src/index.js
+++ books/src/index.js
@@ -4,6 +4,17 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import "./index.css";
 
+const BookList = () => {
+    return (
+        <React.Fragment>
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+        </React.Fragment>
+    );
+}
+
 const Book = () => {
     return (
         <div className="book" >
@@ -30,8 +41,18 @@ const AuthorName = () => {
 const Image = () => {
     return (
         <img style={{ height: 100, width: 100 }} src="/logo192.png" alt="NOT LOADED" />
+        //in style = {{height:100}} first curly braces is for emntering into javas
+        //JavaScripts Mode where as the second braces id for object
+        /*
+            we can also use 
+            styleObject = {
+                height : 100,
+                color : #fff
+            }
+            style = {styleObject}
+        */
     );
 }
 
 
-ReactDom.render(<Book />, document.getElementById('root'));
+ReactDom.render(<BookList />, document.getElementById('root'));
